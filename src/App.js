860fe7440862735c30c46f5168eaea6fbbd781f7
Joseph Elias <jrelias@outlook.com>
2022-03-14 17:56:30 -0400

    added contact form to email owners of other listings

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/PrivateRoute';
 import Category from './pages/Category';
 import CreateListing from './pages/CreateListing';
 import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/contact/:landlordId' element={<Contact />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route
             path='/category/:categoryName/:listingId'
