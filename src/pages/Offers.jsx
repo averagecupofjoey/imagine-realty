fcb49361a094617cab0e1f4097ef12acaaeee72a
Joseph Elias <jrelias@outlook.com>
2022-03-18 15:02:24 -0400

    implemented pagination for offers and category

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -9,6 +9,8 @@ import ListingItem from "../components/ListingItem"
 function Offers() {
   const [listings, setListings] = useState(null)
   const [loading, setLoading] = useState(true)
+  const [lastFetchedListing, setLastFetchedListing] = useState(null)
+
 
   const params = useParams()
 
@@ -19,10 +21,14 @@ function Offers() {
         const listingRef = collection(db, 'listings')
 
         // Create a query
-        const q = query(listingRef, where('offer', '==', true), orderBy('timestamp', 'desc'), limit(10))
+        const q = query(listingRef, where('offer', '==', true), orderBy('timestamp', 'desc'), limit(1))
 
         //Execute query
         const querySnap = await getDocs(q)
+
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
+
 
         let listings = []
         querySnap.forEach((doc)=>{
@@ -42,6 +48,37 @@ function Offers() {
     fetchListings()
   }, [])
 
+  // Pagination / Load more
+ const onFetchMoreListings = async()=>{
+      try {
+        //Get reference
+        const listingRef = collection(db, 'listings')
+
+        // Create a query
+        const q = query(listingRef, where('offer', '==', true), orderBy('timestamp', 'desc'), startAfter(lastFetchedListing), limit(1))
+
+        //Execute query
+        const querySnap = await getDocs(q)
+
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1]
+        setLastFetchedListing(lastVisible)
+
+        let listings = []
+        querySnap.forEach((doc)=>{
+          return listings.push({
+            id: doc.id,
+            data: doc.data()
+          })
+        })
+
+        setListings((prevState)=> [...prevState, ...listings])
+        setLoading(false)
+
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+    }
+
   return (
     <div className="category">
       <header>
@@ -57,6 +94,12 @@ function Offers() {
           ))}
         </ul>
       </main>
+
+      <br />
+      <br />
+      {lastFetchedListing && (
+        <p className="loadMore" onClick={onFetchMoreListings}>Load More</p>
+      )}
       </> : <p>No offers currently available</p>}
     </div>
   )
