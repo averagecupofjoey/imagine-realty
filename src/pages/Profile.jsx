ffd226af8221da147498a60c4ed9cbee64eca742
Joseph Elias <jrelias@outlook.com>
2022-03-18 16:36:44 -0400

    added in edit functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -91,6 +91,10 @@ function Profile() {
     }
   }
 
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/${listingId}`)
+  }
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -122,7 +126,7 @@ function Profile() {
           <p className="listingText">Your listings</p>
           <ul className="listingsList">
             {listings.map((listing)=>(
-              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={()=> onDelete(listing.id)}/>
+              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={()=> onDelete(listing.id)} onEdit={()=> onEdit(listing.id)}/>
             ))}
           </ul>
           </>
